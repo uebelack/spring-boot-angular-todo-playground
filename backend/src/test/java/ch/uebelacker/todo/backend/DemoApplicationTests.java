edd7141f9a57610ab469960bcca362f9a83e028c
David Uebelacker <david@uebelacker.ch>
2023-04-10 17:15:52 +0200

    :tada: initialization

---
+++ backend/src/test/java/ch/uebelacker/todo/backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.uebelacker.todo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
